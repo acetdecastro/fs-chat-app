01ba4aa74d797fb77c39f91d318bad7008c71a2e
acetdecastro <acetdecastro@gmail.com>
2020-10-27 21:01:39 +0800

    Add Geolocation API

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,50 @@
+// eslint-disable-next-line no-undef
 const socket = io();
+const messageForm = document.querySelector('#messageForm');
+const sendLocationButton = document.querySelector('#sendLocationButton');
+
+socket.on('receiveMessage', (message) => {
+  console.log(message);
+});
+
+socket.on('aNewUserHasJoined', (message) => {
+  console.log(message);
+});
+
+socket.on('aUserHasDisconnected', (message) => {
+  console.log(message);
+});
+
+socket.on('aUserSentCoords', (url) => {
+  console.log(url);
+});
+
+socket.on('aUserDeniedGeolocationRequest', (message) => {
+  console.log(message);
+});
+
+messageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const message = e.target.elements.messageInputText.value;
+
+  socket.emit('sendMessage', message);
+});
+
+sendLocationButton.addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    // eslint-disable-next-line no-alert
+    return alert('Geolocation is not support by your browser.');
+  }
+
+  return navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  }, (error) => {
+    if (error.PERMISSION_DENIED) {
+      socket.emit('aUserDeniedGeolocationRequest', 'User denied the request for Geolocation.');
+    }
+  }, { enableHighAccuracy: true });
+});
--- src/index.js
+++ src/index.js
@@ -14,8 +14,29 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 app.use(morgan('dev'));
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
   console.log('New WebSocket connection');
+
+  const welcomeMessage = 'Welcome to the chat 🍔';
+
+  socket.emit('receiveMessage', welcomeMessage);
+  socket.broadcast.emit('aNewUserHasJoined', 'A new user has joined');
+
+  socket.on('sendMessage', (messageFromClient) => {
+    io.emit('receiveMessage', messageFromClient);
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('aUserHasDisconnected', 'A user has disconnected.');
+  });
+
+  socket.on('sendLocation', (coords) => {
+    io.emit('aUserSentCoords', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+  });
+
+  socket.on('aUserDeniedGeolocationRequest', (message) => {
+    io.emit('aUserDeniedGeolocationRequest', message);
+  });
 });
 
 server.listen(port, () => {
